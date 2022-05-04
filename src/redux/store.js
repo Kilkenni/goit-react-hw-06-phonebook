ad54abcb443c9356618352d610dfce9b4ba94e4d
Kilkenni <hpittman89@outlook.com>
2022-05-04 03:39:33 +0300

    + contacts persisting in localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
+import { persistStore, persistReducer } from 'redux-persist';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+import contactsReducer from "./contacts";
 import itemsReducer from "./contacts/items";
 import filterReducer from "./contacts/filter";
 
@@ -10,22 +15,29 @@ const DUMMY_CONTACTS = [
   {id: 'TyphoonMaster', name: 'Vasili Shevchenko', number: '227-91-26'},
 ];
 
-// const contactsReducer = combineReducers({
-//   items: itemsReducer,
-//   filter: filterReducer,
-// });
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: combineReducers({
-      items: itemsReducer,
-      filter: filterReducer,
-    }),
+    contacts: persistedContactsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   preloadedState: {
     contacts: {
-      items: DUMMY_CONTACTS,
+      //items: DUMMY_CONTACTS,
       filter: "",
     }
   },
-})
+});
+
+export const persistor = persistStore(store);
